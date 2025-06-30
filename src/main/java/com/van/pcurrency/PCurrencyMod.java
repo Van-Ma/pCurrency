b071b8c2cf6c07d6f01dbfd36f92f3ac91418371
Van Ma <vaaanma@gmail.com>
2025-06-30 12:31:35 -0500

    revert PCurrencyMod.java

--- src/main/java/com/van/pcurrency/PCurrencyMod.java
+++ src/main/java/com/van/pcurrency/PCurrencyMod.java
@@ -3,14 +3,6 @@ package com.van.pcurrency;
 import com.van.pcurrency.block.ModBlocks;
 import com.van.pcurrency.item.ModItems;
 import net.minecraftforge.fml.common.Mod;
-<<<<<<< HEAD
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-
-@Mod(PCurrencyMod.MODID)
-public class PCurrencyMod {
-    public static final String MODID = "pcurrency";
-=======
 import com.van.pcurrency.entities.ModEntities;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -18,19 +10,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(PCurrencyMod.MOD_ID)
 public class PCurrencyMod {
     public static final String MOD_ID = "pcurrency";
->>>>>>> ec82b245e2930de712c40bc1537a560b96d5c915
 
     public PCurrencyMod() {
    
         
         ModBlocks.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ModItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-<<<<<<< HEAD
-=======
         ModEntities.ENTITY_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
 
 
->>>>>>> ec82b245e2930de712c40bc1537a560b96d5c915
     }
     
 }
